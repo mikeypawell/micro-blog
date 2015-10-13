a016148e8609d739e67ca5391e4416c0cb25c902
mikey-pawell <mikeypawell@gmail.com>
2015-10-12 22:36:23 -0700

    inital commit

--- base.js
+++ base.js
@@ -2,6 +2,36 @@ console.log("Sanity Check: JS is working!");
 
 $(document).ready(function(){
 
-  // code in here
+var main = function() {
+  $('.btn').click(function() {
+    var post = $('.status-box').val();
+    $('<li>').text(post).prependTo('.posts');
+    $('.status-box').val('');
+    $('.counter').text('140');
+    $('.btn').addClass('disabled');
+
+  });
+  }
+  
+  $('.status-box').keypress(function() {
+    var postLength = $(this).val().length;
+    var charactersLeft = 140 - postLength;
+    $('.counter').text(charactersLeft);
+  
+    if(charactersLeft < 0) {
+      $('.btn').addClass('disabled'); 
+    }
+    else {
+      $('.btn').removeClass('disabled');
+    }
+  });
+  
+  $('.btn').addClass('disabled');
+
+
+$(document).ready(main); 
+
 
 });
+
+
