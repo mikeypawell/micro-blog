a4bbe3902f7bf224bac940fb639d4d58ef6452f7
mikey-pawell <mikeypawell@gmail.com>
2015-10-12 22:40:07 -0700

    submission commit

--- base.js
+++ base.js
@@ -2,34 +2,34 @@ console.log("Sanity Check: JS is working!");
 
 $(document).ready(function(){
 
-var main = function() {
-  $('.btn').click(function() {
-    var post = $('.status-box').val();
-    $('<li>').text(post).prependTo('.posts');
-    $('.status-box').val('');
-    $('.counter').text('140');
-    $('.btn').addClass('disabled');
+	var main = function() {
+		$('.btn').click(function() {
+			var post = $('.status-box').val();
+			$('<li>').text(post).prependTo('.posts');
+			$('.status-box').val('');
+			$('.counter').text('140');
+			$('.btn').addClass('disabled');
 
-  });
-  }
-  
-  $('.status-box').keypress(function() {
-    var postLength = $(this).val().length;
-    var charactersLeft = 140 - postLength;
-    $('.counter').text(charactersLeft);
-  
-    if(charactersLeft < 0) {
-      $('.btn').addClass('disabled'); 
-    }
-    else {
-      $('.btn').removeClass('disabled');
-    }
-  });
-  
-  $('.btn').addClass('disabled');
+		});
+	}
+	
+	$('.status-box').keypress(function() {
+		var postLength = $(this).val().length;
+		var charactersLeft = 140 - postLength;
+		$('.counter').text(charactersLeft);
+		
+		if(charactersLeft < 0) {
+			$('.btn').addClass('disabled'); 
+		}
+		else {
+			$('.btn').removeClass('disabled');
+		}
+	});
+	
+	$('.btn').addClass('disabled');
 
 
-$(document).ready(main); 
+	$(document).ready(main); 
 
 
 });
